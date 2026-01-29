3326af91320b045be614205b23f439a99f5d6d3e
Trupti1903 <your@email.com>
2026-01-29 08:22:52 +0530

    created a new file named "hook1.jsx" in src

---
+++ my-app/src/hook1.jsx
@@ -0,0 +1,9 @@
+// Create a registration form with following details
+// 1. Name (Text)
+// 2. Email (Text)
+// 3. Password (Text, Regular Expression)
+
+
+
+// without using form, manage or manipulate the individual components
+// keep the error messages for each field separately and beside the respective field with red colour 
